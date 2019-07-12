b6256830a46417fb7f9f1de9f5cee6572fa6e742
instantchalice <chalice.young@us.sogeti.com>
2019-07-12 15:12:49 -0500

    Add files via upload
    Adding simple object constructs

---
+++ GeoLocation.cs
@@ -0,0 +1,177 @@
+﻿using System;
+
+namespace Utilities
+{
+    public sealed class GeoLocation
+    {
+        private double _degrees;
+        private double _minutes;
+        private double _seconds;
+        private decimal? _coordinate;
+        private Direction _direction;
+
+        public GeoLocation() { }
+
+        public GeoLocation(decimal? coordinate)
+        {
+            Coordinate = coordinate;
+        }
+
+        public GeoLocation(decimal? coordinate, CoordinateUnit coordinateUnit)
+        {
+            Coordinate = coordinate;
+            GetDirection(coordinateUnit);
+        }
+
+        private void GetDirection(CoordinateUnit coordinateUnit)
+        {
+            if ( _coordinate == null )
+            {
+                return;
+            }
+
+            if ( _coordinate > 0)
+            {
+                _direction = coordinateUnit == CoordinateUnit.Latitude ? Direction.North : Direction.East;
+            }
+            else
+            {
+                _direction = coordinateUnit == CoordinateUnit.Latitude ? Direction.South : Direction.West;
+            }
+        }
+
+        public double Degrees
+        {
+            get => _degrees;
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _degrees = value;
+
+                OnPropertyChanged();
+                SetCoordinateValue();
+            }
+        }
+
+        public double Minutes
+        {
+            get => _minutes;
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _minutes = value;
+                OnPropertyChanged();
+                SetCoordinateValue();
+            }
+        }
+
+        public double Seconds
+        {
+            get => _seconds;
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                _seconds = value;
+                OnPropertyChanged();
+                SetCoordinateValue();
+            }
+        }
+
+        public Direction Direction
+        {
+            get => _direction;
+            set
+            {
+                _direction = value;
+                OnPropertyChanged();
+                SetCoordinateValue();
+            }
+        }
+
+        public decimal? Coordinate
+        {
+            get => _coordinate;
+            set
+            {
+                _coordinate = value;
+                OnPropertyChanged();
+                SetComponentValues();
+            }
+        }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+            return $"{Math.Round(Degrees, 2)}\u00b0 {Minutes}' {Seconds}'' {Direction.GetDescription()}";
+        }
+
+        #endregion
+
+        private void SetComponentValues()
+        {
+            if (!_coordinate.HasValue)
+            {
+                _seconds = 0;
+                _minutes = 0;
+                _degrees = 0;
+            }
+            else
+            {
+                _seconds = (double)Math.Round(_coordinate.Value * 3600);
+                _degrees = Math.Abs(_seconds / 3600);
+                _seconds = Math.Abs(_seconds % 3600);
+                _minutes = _seconds / 60;
+                _seconds %= 60;
+            }
+        }
+
+        private void SetCoordinateValue()
+        {
+            if ((Degrees + Minutes + Seconds).Equals(0))
+            {
+                _coordinate = null;
+                return;
+            }
+
+            var directionValue = _direction == Direction.South || _direction == Direction.West ? -1 : 1;
+            _coordinate = (decimal)(Degrees + Minutes / 60 + Seconds / 3600) * directionValue;
+            ValidateCoordinate();
+        }
+
+        private void ValidateCoordinate()
+        {
+            var isLongitude = _direction == Direction.West || _direction == Direction.East;
+            var isLatitude = _direction == Direction.North || _direction == Direction.South;
+
+            if (_coordinate > 180 && isLongitude)
+            {
+                _coordinate = 180;
+            }
+
+            if (_coordinate < -180 && isLongitude)
+            {
+                _coordinate = -180;
+            }
+
+            if (_coordinate > 90 && isLatitude)
+            {
+                _coordinate = 90;
+            }
+
+            if (_coordinate < -90 && isLatitude)
+            {
+                _coordinate = -90;
+            }
+        }
+    }
+}
---
+++ IpAddress.cs
@@ -0,0 +1,135 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Utilities
+{
+    public sealed class IpAddress
+    {
+        private byte? _partA;
+        private byte? _partB;
+        private byte? _partC;
+        private byte? _partD;
+
+        [JsonConstructor]
+        public IpAddress() {}
+
+        public IpAddress(byte? partA, byte? partB, byte? partC, byte? partD)
+        {
+            this.PartA = partA;
+            this.PartB = partB;
+            this.PartC = partC;
+            this.PartD = partD;
+        }
+
+        public IpAddress(string ipAddress)
+        {
+            var ip = IPAddress.Parse(ipAddress).GetAddressBytes();
+            this.PartA = ip[0];
+            this.PartB = ip[1];
+            this.PartC = ip[2];
+            this.PartD = ip[3];
+        }
+
+        [Exportable]
+        public byte? PartA
+        {
+            get => _partA;
+            set
+            {
+                _partA = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IpFullString));
+            }
+        }
+
+        [Exportable]
+        public byte? PartB
+        {
+            get => _partB;
+            set
+            {
+                _partB = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IpFullString));
+            }
+        }
+
+        [Exportable]
+        public byte? PartC
+        {
+            get => _partC;
+            set
+            {
+                _partC = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IpFullString));
+            }
+        }
+
+        [Exportable]
+        public byte? PartD
+        {
+            get => _partD;
+            set
+            {
+                _partD = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IpFullString));
+            }
+        }
+
+        public string IpFullString => this.CompletePart(PartA) + "." +
+                                      this.CompletePart(PartB) + "." +
+                                      this.CompletePart(PartC) + "." +
+                                      this.CompletePart(PartD);
+
+        public override bool IsValid => PartAIsValid && PartBIsValid && PartCIsValid && PartDIsValid;
+
+        public bool PartAIsValid => PartA.HasValue && PartA.Value > 0 && PartA.Value < 255;
+        public bool PartBIsValid => PartB.HasValue && PartB.Value <= 255;
+        public bool PartCIsValid => PartC.HasValue && PartC.Value <= 255;
+        public bool PartDIsValid => PartD.HasValue && PartD.Value <= 255;
+
+        public override string ToString()
+        {
+            return this.IpFullString;
+        }
+
+        public IPAddress ToSystemIpAddress()
+        {
+            return IpFullString == null || IpFullString == "..." ? null : IPAddress.Parse(IpFullString);
+        }
+
+        private string CompletePart(byte? part)
+        {
+            if (!part.HasValue)
+            {
+                return string.Empty;
+            }
+
+            var value = part.ToString();
+            if (value.Length == 3)
+            {
+                return value;
+            }
+
+            if (value.Length == 2)
+            {
+                return "0" + value;
+            }
+
+            if (value.Length == 1)
+            {
+                return "00" + value;
+            }
+
+            return string.Empty;
+        }
+
+        public IpAddress Clone()
+        {
+            var address = new IpAddress(this.IpFullString);
+            return address;
+        }
+    }
+}
